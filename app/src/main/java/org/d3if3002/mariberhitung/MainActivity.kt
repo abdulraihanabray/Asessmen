e5246c679092c7ad3f11a409806a236c2ad7c73b
abray <abdulraihan@gmail.com>
2023-03-30 21:58:08 +0700

    assesmen1

---
+++ app/src/main/java/org/d3if3002/mariberhitung/MainActivity.kt
@@ -0,0 +1,53 @@
+package org.d3if3002.mariberhitung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import org.d3if3002.mariberhitung.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnHitung.setOnClickListener(){
+
+            if(binding.etAngka1.text.toString() == ""){
+                binding.etAngka1.error = "Angka 1 wajib diisi ya"
+                return@setOnClickListener
+            }
+            if(binding.etAngka2.text.toString() == ""){
+                binding.etAngka2.error = "Angka 2 wajib diisi ya"
+                return@setOnClickListener
+            }
+
+
+            val nilai_angka1 = binding.etAngka1.text.toString().toInt()
+            val nilai_angka2 = binding.etAngka2.text.toString().toInt()
+
+            binding.tvHasil.text = do_hitung_hasil(nilai_angka1, nilai_angka2)
+        }
+    }
+
+    private fun do_hitung_hasil(nilai1:Int,nilai2:Int):String{
+        var hitung_hasil:Int = 0
+        val pilih_operator = binding.spAritmatika.selectedItem.toString()
+
+        when(pilih_operator){
+            "x" -> hitung_hasil = nilai1 * nilai2
+            "/" -> hitung_hasil = nilai1 / nilai2
+            "+" -> hitung_hasil = nilai1 + nilai2
+            "-" -> hitung_hasil = nilai1 - nilai2
+            else -> {
+                Toast.makeText(this, "Pilih operatormu", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val result_hasil = hitung_hasil
+        return result_hasil.toString()
+    }
+}
